75da8f264f8d31e9f21298fcfb63a7635b8ecdc0
Erick Carranza Meza <erickcarranzameza@gmail.com>
2021-05-29 23:19:39 -0500

    Header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
 
@@ -7,15 +7,31 @@ import { AuthService } from '../../services/auth.service';
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, AfterViewInit {
+
+  id: string;
+  local: string;
+  rol: string;
 
   constructor( private auth: AuthService, private router: Router) { }
 
   ngOnInit(): void {
+    this.id = sessionStorage.getItem('idUsuario');    
+    this.local = sessionStorage.getItem('local');
+    this.rol = sessionStorage.getItem('rol');
   }
+
+  ngAfterViewInit(){
+
+  }
+
   salir(){
     this.auth.Logout();
     this.router.navigateByUrl('/login');
+  }
+
+  guardarLocal(propio: string): void {
+    sessionStorage.setItem('local', propio);
   }
 
 }
